156ceb3b0a454e98f1965a47e3e6b3de8522e227
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-09-23 10:40:15 +0900

    190923|Homepage 생성 시작

--- frontend/src/components/pages/HomePage.vue
+++ frontend/src/components/pages/HomePage.vue
@@ -1,9 +1,39 @@
 <template>
   <v-flex shrink>
     <!-- <span class="display-3 grey--text">Select menu</span> -->
+
+    <!-- 따져아 할 상태 : 로그인and구독 / 로그인and구독X / 로그인X -->
+
+    <!--  -->
+
+    <!--  -->
+
+    <!--  -->
+
+
   </v-flex>
 </template>
 
 <script>
-export default {}
+export default {
+  components:{
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.usercheck()
+  },
+  methods : {
+    usercheck() {
+      console.log(this.$session.get('id'))
+
+
+    }
+  },
+
+}
 </script>
