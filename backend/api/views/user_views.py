fae70ec21f3d72eb3175dd9bdddcabb308a964dc
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-09-30 10:48:48 +0900

    190930|새로 회원가입 한 경우 유사유저 추천 수정중

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -53,56 +53,56 @@ def detail(request, user_id):
         k = clu.n_component
         way = clu.way
         clusters = []
-        if user_profile.pk <= 6041:
-            if way=="K":
-                if(k==3):
-                    cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K3
-                    clusters = User_Cluster_Kmeans.objects.filter(K3=cluster_num)
-                elif(k==4):
-                    cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K4
-                    clusters = User_Cluster_Kmeans.objects.filter(K4=cluster_num)
-                elif(k==5):
-                    cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K5
-                    clusters = User_Cluster_Kmeans.objects.filter(K5=cluster_num)
-                elif(k==6):
-                    cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K6
-                    clusters = User_Cluster_Kmeans.objects.filter(K6=cluster_num)
-                else:
-                    cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K7
-                    clusters = User_Cluster_Kmeans.objects.filter(K7=cluster_num)
-            elif way=="H":
-                if(k==3):
-                    cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H3
-                    clusters = User_Cluster_Hmeans.objects.filter(H3=cluster_num)
-                elif(k==4):
-                    cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H4
-                    clusters = User_Cluster_Hmeans.objects.filter(H4=cluster_num)
-                elif(k==5):
-                    cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H5
-                    clusters = User_Cluster_Hmeans.objects.filter(H5=cluster_num)
-                elif(k==6):
-                    cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H6
-                    clusters = User_Cluster_Hmeans.objects.filter(H6=cluster_num)
-                else:
-                    cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H7
-                    clusters = User_Cluster_Hmeans.objects.filter(H7=cluster_num)
+        print(k, way, user_id)
+        if way=="K":
+            if(k==3):
+                cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K3
+                clusters = User_Cluster_Kmeans.objects.filter(K3=cluster_num)
+            elif(k==4):
+                cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K4
+                clusters = User_Cluster_Kmeans.objects.filter(K4=cluster_num)
+            elif(k==5):
+                cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K5
+                clusters = User_Cluster_Kmeans.objects.filter(K5=cluster_num)
+            elif(k==6):
+                cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K6
+                clusters = User_Cluster_Kmeans.objects.filter(K6=cluster_num)
             else:
-                if(k==3):
-                    cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM3
-                    clusters = User_Cluster_EM.objects.filter(EM3=cluster_num)
-                elif(k==4):
-                    cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM4
-                    clusters = User_Cluster_EM.objects.filter(EM4=cluster_num)
-                elif(k==5):
-                    cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM5
-                    clusters = User_Cluster_EM.objects.filter(EM5=cluster_num)
-                elif(k==6):
-                    cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM6
-                    clusters = User_Cluster_EM.objects.filter(EM6=cluster_num)
-                else:
-                    cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM7
-                    clusters = User_Cluster_EM.objects.filter(EM7=cluster_num)
-
+                cluster_num = User_Cluster_Kmeans.objects.get(UserID=user_id).K7
+                clusters = User_Cluster_Kmeans.objects.filter(K7=cluster_num)
+        elif way=="H":
+            if(k==3):
+                cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H3
+                clusters = User_Cluster_Hmeans.objects.filter(H3=cluster_num)
+            elif(k==4):
+                cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H4
+                clusters = User_Cluster_Hmeans.objects.filter(H4=cluster_num)
+            elif(k==5):
+                cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H5
+                clusters = User_Cluster_Hmeans.objects.filter(H5=cluster_num)
+            elif(k==6):
+                cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H6
+                clusters = User_Cluster_Hmeans.objects.filter(H6=cluster_num)
+            else:
+                cluster_num = User_Cluster_Hmeans.objects.get(UserID=user_id).H7
+                clusters = User_Cluster_Hmeans.objects.filter(H7=cluster_num)
+        else:
+            if(k==3):
+                cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM3
+                clusters = User_Cluster_EM.objects.filter(EM3=cluster_num)
+            elif(k==4):
+                cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM4
+                clusters = User_Cluster_EM.objects.filter(EM4=cluster_num)
+            elif(k==5):
+                cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM5
+                clusters = User_Cluster_EM.objects.filter(EM5=cluster_num)
+            elif(k==6):
+                cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM6
+                clusters = User_Cluster_EM.objects.filter(EM6=cluster_num)
+            else:
+                cluster_num = User_Cluster_EM.objects.get(UserID=user_id).EM7
+                clusters = User_Cluster_EM.objects.filter(EM7=cluster_num)
+        print(clusters, 11111111111111111111111111)
         profiles = []
         my_cluster = []
         serializer = ProfileSerializer(user_profile)
--- frontend/src/components/pages/HomePage.vue
+++ frontend/src/components/pages/HomePage.vue
@@ -128,6 +128,7 @@ export default {
         const id = this.$session.get('id_number')
         var profile = await axios.get(`${apiUrl}/users/${id}`)
         this.profile_data = profile.data
+        console.log(profile)
         // 구독 날짜 확인하기,
         // 오늘 날짜 : this.now_date , ex) 20190910
         // 회원의 구독 날짜 : this.subscription_date, ex) 20190801
